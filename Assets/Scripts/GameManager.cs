d5a75c0ff47324eade4627cb0759c3c0dcc8c8ee
PaulElcampeon <pauloladele@live.co.uk>
2021-01-30 00:50:15 +0000

    Added TestMovement and Ememy script.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool _isGameOver = false;
+
+    private bool _isControlsDisabled = false;
+
+    public static GameManager INSTANCE;
+
+    void Start()
+    {
+        INSTANCE = this;    
+    }
+
+    void Update()
+    {
+        if (_isGameOver)
+        {
+            Debug.Log("Game over");
+        }
+        
+    }
+
+    public void SetIsGameover(bool isGameOver)
+    {
+        this._isGameOver = isGameOver;
+    }
+
+    public void DisableControls()
+    {
+        Debug.Log("Controls disabled");
+
+        this._isControlsDisabled = true;
+    }
+
+    public bool IsControlsDisabled()
+    {
+        return this._isControlsDisabled;
+    }
+}
