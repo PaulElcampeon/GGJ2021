7258005c2a08455504a5d5b48b869459a570a9b3
OneOverCosine <TheCandyDragon@gmail.com>
2021-01-30 16:42:36 +0000

    Added settings menu
    Also put the pause menu back in its own script

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,12 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Menus : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
-
-    /* Will likely delete this as PauseMenu has all the same stuff - 
-    I wanted to try and put all the menus in one place, thinking they'd
-    have a lot of the same functionality...*/
     public static bool gameIsPaused = false;
     public GameObject pauseMenuUI;
 
