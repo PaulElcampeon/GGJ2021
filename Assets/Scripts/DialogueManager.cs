31c434c1e5d71129d5f1bd4f160506cfea3a98f0
isabela <isabela@HarbourVR.com>
2021-01-31 15:22:33 +0000

    Dialogue Manager Done

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -2,9 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
+    
     private Queue <string> sentences;
 
     void Start()
@@ -16,10 +20,11 @@ public class DialogueManager : MonoBehaviour
     public void StartDialogue(Dialogue dialogue)
     {
         //we can trigger dialogue in different ways in here. 
-        Debug.Log("Starting conversation with" + dialogue.name);
 
+        nameText.text = dialogue.name;
         sentences.Clear(); //clears sentences from previous conversation
 
+        //adds the sentences to a queue
         foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
@@ -30,6 +35,7 @@ public class DialogueManager : MonoBehaviour
 
     public void DisplayNextSentence()
     {
+        //if the sentence count is equal to 0, we end the dialogue
         if (sentences.Count == 0)
         {
             EndDialogue();
@@ -37,7 +43,7 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        dialogueText.text = sentence;
     }
 
     void EndDialogue()
